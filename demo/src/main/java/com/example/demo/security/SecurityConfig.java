8b5fb6abc1afcc6a06eed1a0da764e9ace05a1b2
Kamal Ourajdal <kamalourajdal@gmail.com>
2026-08-13 16:25:08 +0100

    Add SecurityConfig class to configure Spring Security with in-memory user details and custom access handling

---
+++ demo/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.demo.security;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+            User.withUsername("user1").password("{noop}1234").roles("USER").build(),
+            User.withUsername("admin").password("{noop}1234").roles("ADMIN").build(),
+            User.withUsername("user2").password("{noop}1234").roles("ADMIN", "USER").build()
+        );
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .formLogin(Customizer.withDefaults())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/403.html").permitAll()
+                .anyRequest().authenticated()
+            )
+            .exceptionHandling(exceptionHandling -> exceptionHandling
+                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                    response.setStatus(HttpStatus.FORBIDDEN.value());
+                    request.getRequestDispatcher("/403.html").forward(request, response);
+                })
+            );
+            
+
+        return httpSecurity.build();
+    }
+}
