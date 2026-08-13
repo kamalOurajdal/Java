e4930874e0c45a7e29d076be1826ab4592510314
Kamal Ourajdal <kamalourajdal@gmail.com>
2026-08-13 22:50:46 +0100

    Update form login configuration in SecurityConfig to set default success URL

--- demo/src/main/java/com/example/demo/security/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.demo.security;
 import org.springframework.http.HttpStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,7 +27,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-            .formLogin(Customizer.withDefaults())
+            .formLogin(form -> form.defaultSuccessUrl("/", true))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/403.html").permitAll()
                 .anyRequest().authenticated()
