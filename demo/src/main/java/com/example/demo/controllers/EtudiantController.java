0546d085512c44d33f613abb1661d6895ad5cb97
Kamal Ourajdal <kamalourajdal@gmail.com>
2026-08-13 16:25:14 +0100

    Add new endpoints in EtudiantController with security annotations for user and admin roles

--- demo/src/main/java/com/example/demo/controllers/EtudiantController.java
+++ demo/src/main/java/com/example/demo/controllers/EtudiantController.java
@@ -2,7 +2,10 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Repository.EtudiantRepo;
@@ -22,5 +25,25 @@ public class EtudiantController {
         List<Etudiant> etudiants = this.etudiantRepo.findAll();
         return etudiants;
     }
+
+    @GetMapping("")
+    public String indString() {
+        return "hello world";
+    }
+
+    @GetMapping("/users/{id}")
+    @PreAuthorize("hasRole('USER')")
+    @PostAuthorize("returnObject == #id")
+    public Long usersIndex(@PathVariable Long id) {
+        return 2L;
+    }
+
+    @GetMapping("/admin/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostAuthorize("returnObject.id == #id")
+    public Long adminIndex(@PathVariable Long id) {
+        return 2L;
+    }
+    
     
 }
